d13ab72c72339081ac89975b3cc755c565d437e4
unknown <tharunsitharunsi1@gmail.com>
2024-07-26 00:10:50 +0530

    hello

---
+++ index.js
@@ -0,0 +1,72 @@
+
+// In var both reassigning and redeclaring is applicable
+//var a = 10;  //assigning or declaring
+//console.log(typeof a);
+//console.log(a);
+//
+//a=20;   // variable reassigning
+//console.log(a);
+//
+//var a=20;  // variable redeclaring
+//console.log(a);
+
+// In let only reassigning is applicable
+
+//let b=10;
+//console.log(typeof b);
+//console.log(b);
+//
+//b=20;
+//console.log(b);
+
+// let b-20; will not work
+
+//when we use const  ressigning and redeclaration will not work      
+
+
+// types of scope
+
+// global scope is possible for all var.let,const
+
+//global scope can be accessed from anywhere
+
+//function cannot be accessed outside the fuction
+
+//block
+//
+//let a ="global";
+//
+//function hello(){
+//    let b="function";
+//
+//}
+//
+//hello();
+//
+//{
+//    var c ="block";
+//}
+//    
+//   types of string "" & '' & ``(it is near 1)
+//
+//let a = "thala";
+//let b = 'for a';
+//let c = `reason`;
+//let d = null;
+//let e;    //undefined
+//console.log(a);
+//console.log(b);
+//console.log(c);
+//console.log(d);
+//console.log(e);
+//
+
+   let number = [];  //empty array
+
+   let number1 = [1,2,3,4,5]; // array with value
+
+   let number2 = [1,2];
+   number2.push(3);    // we can push the element later
+   number3.push(4);
+
+   console
